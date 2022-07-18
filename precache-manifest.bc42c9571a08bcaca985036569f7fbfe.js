a2d2ea9f61d3ee3c57966ae0a1321f33ff75874a
Aum Patel <aumrp17@gmail.com>
2022-07-18 12:24:54 +0530

    Updates

---
+++ precache-manifest.bc42c9571a08bcaca985036569f7fbfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad5d6b45b2d85d817118",
+    "url": "/robofriends/static/css/main.77237763.chunk.css"
+  },
+  {
+    "revision": "ad5d6b45b2d85d817118",
+    "url": "/robofriends/static/js/main.ad5d6b45.chunk.js"
+  },
+  {
+    "revision": "ee112eb61b49e60a8b07",
+    "url": "/robofriends/static/css/1.ba15efb2.chunk.css"
+  },
+  {
+    "revision": "ee112eb61b49e60a8b07",
+    "url": "/robofriends/static/js/1.ee112eb6.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c5d976bdc8da81ff40cae5eb204bc5ab",
+    "url": "/robofriends/index.html"
+  }
+];
